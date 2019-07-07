2136d4de751e49b925536f33d71a8e75a9bc37d9
sallybaker <sallybaker@users.noreply.github.com>
2019-07-07 22:04:35 +1000

    completed triangle exceptions

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	raise TriangleError, "Not valid triangle dimensions" unless (a > 0) && (b > 0) && (c >0)
+	raise TriangleError, "Not valid triangle dimensions 2" unless (a+b > c) && (a+c > b) && (b+c > a)
   if (a == b) && (a == c) && (b == c) 
   	result = :equilateral
   elsif (a != b) && (a != c) && (b != c)
